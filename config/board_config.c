6830404382197f66e21699ed131852abf136441a
Nicolas Jeker <n.jeker@gmx.net>
2013-12-08 18:48:58 +0100

    revert unwanted changes

--- config/board_config.c
+++ config/board_config.c
@@ -6,31 +6,18 @@
 void init(void)
 {
 	// oscillator config
-	OSCCON = 0x74; // 8 MHz
-	//PLLEN = 1; // enable PLL
+	OSCCON = 0x77; // 8 MHz
 	// input/output
 	TRISA = 0x0C;
 	TRISB = 0x00;
 	TRISC = 0x80;
-	// no analog pls
-	ANCON0 = 0xFF;
-	ANCON1 = 0x31;
-	// clear output latch
+	// clear outputs / enable pull-ups (latch)
 	LATA = 0;
 	LATB = 0x3F;
 	LATC = 0;
-
-	// 0, 0101 (postscaler 6), 0 (off), 01 (prescaler 4)
-	T2CON = 0b00101001;
-	TMR2 = 0;
-	// timer 2 init (76kHz)
-	PR2 = 26;
-	TMR2IE = 1;
-	TMR2ON = 1;
-	TMR2IP = 1;
 	// enable interupts
-	PEIE = 1;
 	GIE = 1;
+	PEIE = 1;
 }
 
 /**
--- main.c
+++ main.c
@@ -21,10 +21,6 @@ void interrupt isr()
 	if (TMR2IF && TMR2IE)
 	{
 		TMR2IF = 0;
-		
-		OUT_3 = !OUT_3;
-		
-		/*
 		if (burst)
 		{
 			IR2 = !IR2;
@@ -36,7 +32,6 @@ void interrupt isr()
 			antiburst = 0;
 			burst = !burst;
 		}
-		*/
 		return;
 	}
 }
@@ -46,7 +41,32 @@ void interrupt isr()
  */
 int main()
 {
-	init();
+	// oscillator config
+	OSCCON = 0x74; // 8 MHz
+	PLLEN = 1; // enable PLL
+	// input/output
+	TRISA = 0x0C;
+	TRISB = 0x00;
+	TRISC = 0x80;
+	// no analog pls
+	ANCON0 = 0xFF;
+	ANCON1 = 0x31;
+	// clear output latch
+	LATA = 0;
+	LATB = 0x3F;
+	LATC = 0;
+
+	// 0, 0101 (postscaler 6), 0 (off), 01 (prescaler 4)
+	T2CON = 0x00;
+	TMR2 = 0;
+	// timer 2 init (76kHz)
+	PR2 = 26;
+	// TMR2IE = 1;
+	TMR2ON = 1;
+	TMR2IP = 1;
+	// enable interupts
+	PEIE = 1;
+	GIE = 1;
 	
 	RELAIS = 1;
 	
